d88a0bcbe06a8fd981ddcf1f72f7d810956da182
Gelato-B2B <105652074+Gelato-B2B@users.noreply.github.com>
2023-11-17 19:29:52 +0400

    Update Liquorice.sol

--- Liquorice.sol
+++ Liquorice.sol
@@ -8,54 +8,55 @@ contract Liquorice is ReentrancyGuard{
 
     struct LendingPool {
         uint256 interestRate;
-        address borrowingToken;
-        address collateralToken;
+        uint256 capital; //in ETH
         uint256 loanDuration;
         uint256 punishmentFee;
         uint256 collateralRatio;
         address[] whitelist;
-        bool leverageTradingEnabled;
-        address tradingPair;
-        uint256 newCollateralRatio;
-        uint256 newPunishmentFee;
+        address owner;
     }
+
+    address constant borrowETH = address(0);
+    address constant lendUSDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174; // Replace with the actual USDC contract address
 
     mapping(uint256 => LendingPool) public lendingPools;
     uint256 public nextPoolId;
 
     function createLendingPool(
         uint256 _interestRate,
-        address _borrowingToken,
-        address _collateralToken,
+        uint256 _capital,
         uint256 _loanDuration,
         uint256 _punishmentFee,
         uint256 _collateralRatio,
-        address[] memory _whitelist,
-        bool _leverageTradingEnabled,
-        address _tradingPair,
-        uint256 _newCollateralRatio,
-        uint256 _newPunishmentFee
-    ) public {
-        // Add your validation logic here
+        address[] memory _whitelist
+    ) external payable {
+
+        require(msg.value >= _capital, "Not enough ETH provided");
+        require(_punishmentFee <= 100, "Punishment fee cannot exceed 100.");
 
         LendingPool memory newPool = LendingPool({
             interestRate: _interestRate,
-            borrowingToken: _borrowingToken,
-            collateralToken: _collateralToken,
+            capital: _capital,
             loanDuration: _loanDuration,
             punishmentFee: _punishmentFee,
             collateralRatio: _collateralRatio,
             whitelist: _whitelist,
-            leverageTradingEnabled: _leverageTradingEnabled,
-            tradingPair: _tradingPair,
-            newCollateralRatio: _newCollateralRatio,
-            newPunishmentFee: _newPunishmentFee
+            owner: msg.sender
         });
 
         lendingPools[nextPoolId] = newPool;
         nextPoolId++;
     }
 
-    // Additional functions and logic go here
+    function withdrawCapital(uint256 poolId, uint256 withdrawAmount) external {
+        LendingPool storage pool = lendingPools[poolId];
+        require(msg.sender == pool.owner, "Only the pool owner can withdraw capital.");
+        require(withdrawAmount <= pool.capital, "Withdrawl request exceeds pool amount.");
+        pool.capital -= withdrawAmount;
+        payable(pool.owner).transfer(withdrawAmount);
+    }
+
+    
+
 
 }
