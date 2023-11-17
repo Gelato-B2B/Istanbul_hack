f4125533f7f0cb20938206de27f3d248a3767ae4
Gelato-B2B <105652074+Gelato-B2B@users.noreply.github.com>
2023-11-17 23:15:17 +0400

    Update Liquorice.sol

--- Liquorice.sol
+++ Liquorice.sol
@@ -2,13 +2,11 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 interface IERC20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
-contract Liquorice is ReentrancyGuard{
+contract Liquorice {
 
     struct LendingPool {
         uint256 interestRate;
@@ -25,12 +23,54 @@ contract Liquorice is ReentrancyGuard{
         uint256 lockedCollateral;
     }
 
+    struct Order {
+        uint256 amountIn;
+        uint256 amountOut;
+        uint256 validTo;
+        address taker;
+        bytes uid; 
+    }     
+
+    struct liquidationMarket {
+        address lender;
+        address borrower; 
+        uint256 borrowedETH;
+        uint256 lockedColalteral;       
+    }
+
     address constant lockUSDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
 
     mapping(uint256 => LendingPool) public lendingPools;
     mapping(address => borrowers) public poolBorrowers;
+    mapping(bytes32 => bool) public invalidatedOrders;    
 
     uint256 public nextPoolId;
+
+    bytes32 immutable DOMAIN_SEPARATOR; 
+
+    bytes32 constant EIP712DOMAIN_TYPEHASH = keccak256(
+        "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+    ); 
+
+    bytes32 constant ORDER_TYPEHASH = keccak256(
+        "Order(uint256 amountIn,uint256 amountOut,address taker,bytes uid)"
+    );
+
+    constructor() {
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                EIP712DOMAIN_TYPEHASH,
+                keccak256("Liquorice"), // contract name
+                keccak256("1"), // Version
+                block.chainid,
+                address(this)
+            )
+        );
+    }    
+
+    //---------------------------------------------------------------------------------------
+    //------Main functions----------------------------------------------------------
+    //---------------------------------------------------------------------------------------
 
     function createLendingPool(
         uint256 _interestRate,
@@ -83,5 +123,85 @@ contract Liquorice is ReentrancyGuard{
         require(usdcToken.transferFrom(address(this), msg.sender, amount), "Transfer failed.");
     }
 
+    function settlement(Order memory _order, bytes memory _signature) external payable {
+        bytes32 orderhash = _hashOrder(_order);
+        address recsigner = recoverSigner(orderhash, _signature);          
+        require(recsigner == _order.taker, "Ivalid signature");  
+
+        //Checking that quote is not reused
+        _invalidateOrder(orderhash);
+
+        // swap
+
+        payable(msg.sender).transfer(_order.amountIn);
+        IERC20 usdcToken = IERC20(lockUSDC);
+        require(usdcToken.transferFrom(msg.sender, address(this), _order.amountOut), "Transfer failed.");
+    }
+
+
+
+    //---------------------------------------------------------------------------------------
+    //------Supplimentary functions----------------------------------------------------------
+    //---------------------------------------------------------------------------------------
+
+
+
+    function _invalidateOrder(bytes32 _hash) internal {
+        require(!invalidatedOrders[_hash], "Invalid Order");
+        invalidatedOrders[_hash] = true;
+    }           
+    
+   //hashes order data
+    function _hashOrder(Order memory _order) public pure returns (bytes32) {
+        return keccak256(abi.encode(
+            ORDER_TYPEHASH,
+            _order.amountIn,
+            _order.amountOut,            
+            _order.taker,
+            _order.uid
+        ));
+    }
+
+    //generates final hash including domain separator to be signed by maker from received order hash
+    function getEthSignedMessageHash(bytes32 _orderHash)
+        public
+        view
+        returns (bytes32 hash)
+    {
+        return keccak256(abi.encodePacked(
+            "\x19\x01",
+            DOMAIN_SEPARATOR,
+            _orderHash
+        ));
+    }
+
+    //view function that can be called by test maker to generate hash that maker has to sign
+    function generateEIP712Hash(Order memory _order) public view returns (bytes32) {
+        return getEthSignedMessageHash(_hashOrder(_order));
+    } 
+
+    function recoverSigner(
+        bytes32 _hash,
+        bytes memory signature
+    ) public view returns (address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        bytes32 messageHash = getEthSignedMessageHash(_hash);
+
+        assembly {
+            r := mload(add(signature, 32))
+            s := mload(add(signature, 64))
+            v := byte(0, mload(add(signature, 96)))
+        }
+
+        if (v < 27) {
+            v += 27;
+        }
+
+        return ecrecover(messageHash, v, r, s);
+    }       
+
+    receive() external payable {}
 
 }
