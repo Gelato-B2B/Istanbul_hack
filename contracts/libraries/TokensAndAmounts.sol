3a7d5b6f2cb8882a562d4430e2b222bbbd0b6f58
Анатолий Мухин <asmukhin@nes.ru>
2023-12-04 16:37:04 +0400

    Small pragma change

--- contracts/libraries/TokensAndAmounts.sol
+++ contracts/libraries/TokensAndAmounts.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.17;
+pragma solidity 0.8.19;
 
-import "@1inch/solidity-utils/contracts/libraries/AddressLib.sol";
+import '@1inch/solidity-utils/contracts/libraries/AddressLib.sol';
 
 library TokensAndAmounts {
     struct Data {
@@ -10,7 +10,9 @@ library TokensAndAmounts {
         uint256 amount;
     }
 
-    function decode(bytes calldata cd) internal pure returns(Data[] calldata decoded) {
+    function decode(
+        bytes calldata cd
+    ) internal pure returns (Data[] calldata decoded) {
         assembly {
             decoded.offset := cd.offset
             decoded.length := div(cd.length, 0x40)
